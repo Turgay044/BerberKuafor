2909ad877fcd19bd6a2e8980df54b8c96ccd0a41
Turgay044 <turgayturguterkal@gmail.com>
2024-12-26 21:58:57 +0300

    Veritabanı Bağlantısı

---
+++ BerberKuafor/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BerberKuafor.Models
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-LQKJ0N7; database=BerberDB; integrated security = true;");
+        }
+        public DbSet<Personel> Personels { get; set; }
+    }
+}
--- BerberKuafor/Models/Personel.cs
+++ BerberKuafor/Models/Personel.cs
@@ -1,7 +1,10 @@
-﻿namespace BerberKuafor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BerberKuafor.Models
 {
     public class Personel
     {
+        [Key]
         public int IDPersonel { get; set; }
         public string PersonelAdi { get; set; }
         public string PersonelSoyad { get; set; }
